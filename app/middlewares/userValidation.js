84cb4d9f665a169426706b21673a4b68c01fd209
Batman2532 <charjan44@gmail.com>
2021-06-28 23:45:07 +0530

    Added: unique email address

--- app/middlewares/userValidation.js
+++ app/middlewares/userValidation.js
@@ -4,7 +4,7 @@ const userSchema = joi.object({
     firstName: joi.string().min(3).max(15).required(),
     lastName: joi.string().min(3).max(15).required(),
     email: joi.string().email().required(),
-    password: joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required()
+    password: joi.string().pattern(new RegExp('^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$')).required()
 })
 
 module.exports = {userSchema}
